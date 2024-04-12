b9770a560cf773f349218b01ee2e4614d702a6d7
pankajdevops2489 <pankajdevops2489@gmail.com>
2024-04-12 14:31:26 +0530

    Updated all with sample app

--- Terraform-Files/provider.tf
+++ Terraform-Files/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "5.45.0"
     }
   }
+  backend "s3" {
+    bucket = "state-file-s3-queen-python-app"
+     key = "terraform/backend/terraform_aws.tfstate"
+     region = "us-east-1"
+     dynamodb_table = "terraform-s3-backend-locking"
+     encrypt = true
+  }
 }
 
 provider "aws" {
